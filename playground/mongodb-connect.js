32f332a51601f7b5eadbdd7c13ccf89a64010ed8
Yang, Hao <hao-yang@uiowa.edu>
2018-05-09 18:00:48 -0500

    Deleting document

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -13,28 +13,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
   
-//  db.collection('Todos').insertOne({
-//    text: 'Something to do',
-//    completed: false
-//  }, (err, result) => {
-//    if (err) {
-//      console.log('Unable to insert todo', err);
-//    }
-//    
-//    console.log(JSON.stringify(result.ops, undefined, 2));
-//  });
+  db.collection('Todos').insertOne({
+    text: 'Something to do',
+    completed: false
+  }, (err, result) => {
+    if (err) {
+      console.log('Unable to insert todo', err);
+    }
+    
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
   
-//  db.collection('Users').insertOne({
-//    name: 'Hao',
-//    age: 24,
-//    location: 'Iowa City'
-//  }, (err, result) => {
-//    if (err) {
-//      console.log('Unable to insert user', err);
-//    }
-//    
-//    console.log(result.ops[0]._id.getTimestamp());
-//  });
+  db.collection('Users').insertOne({
+    name: 'Hao',
+    age: 24,
+    location: 'Iowa City'
+  }, (err, result) => {
+    if (err) {
+      console.log('Unable to insert user', err);
+    }
+    
+    console.log(result.ops[0]._id.getTimestamp());
+  });
   
   client.close();
 });
