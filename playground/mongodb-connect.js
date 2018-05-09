418cf7ebc78368a1d076c8cf855c5905ca5044c7
Yang, Hao <hao-yang@uiowa.edu>
2018-05-09 17:10:46 -0500

    insert data

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,10 +1,40 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+// ES6 object destructuring
+//var user = {name: 'Hao', age: 24};
+//var {name} = user;
+//console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if (err) {
     console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
   
-  db.close();
+//  db.collection('Todos').insertOne({
+//    text: 'Something to do',
+//    completed: false
+//  }, (err, result) => {
+//    if (err) {
+//      console.log('Unable to insert todo', err);
+//    }
+//    
+//    console.log(JSON.stringify(result.ops, undefined, 2));
+//  });
+  
+//  db.collection('Users').insertOne({
+//    name: 'Hao',
+//    age: 24,
+//    location: 'Iowa City'
+//  }, (err, result) => {
+//    if (err) {
+//      console.log('Unable to insert user', err);
+//    }
+//    
+//    console.log(result.ops[0]._id.getTimestamp());
+//  });
+  
+  client.close();
 });
